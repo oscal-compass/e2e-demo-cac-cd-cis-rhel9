55954390b934a3d5a4df58be6d325d59a2d3beb7
Lou DeGenaro <lou.degenaro@gmail.com>
2025-02-17 13:44:32 -0500

    Fix update ssp script
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/update_ssp.sh
+++ scripts/automation/update_ssp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export COMMIT_TITLE="chore: Component definitions automatic update."
-export COMMIT_BODY="Sync components with ocp-oscal-cd repo"
+export COMMIT_BODY="Sync components with ocp-oscal-ssp repo"
 git config --global user.email "automation@example.com"
 git config --global user.name "AutomationBot" 
 cd ocp-oscal-ssp
@@ -21,7 +21,7 @@ else
      echo "Nothing to commit" 
   else
      git commit -m "$COMMIT_TITLE"
-     remote=https://$GITHUB_TOKEN@github.com/oscal-compass/e2e-demo-cac-cd-cis-ssp
+     remote=https://$GITHUB_TOKEN@github.com/oscal-compass/e2e-demo-cac-ssp-cis
      git push -u "$remote" "cd_autoupdate_$GITHUB_RUN_ID"
      echo $COMMIT_BODY
      gh pr create -t "$COMMIT_TITLE" -b "$COMMIT_BODY" -B "develop" -H "cd_autoupdate_$GITHUB_RUN_ID" 
